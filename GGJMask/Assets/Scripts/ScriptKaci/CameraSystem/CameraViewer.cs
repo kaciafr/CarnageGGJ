0880e11f8f8b7ffb0c58875e873a102a80eebbb0
kaciafr <162875953+kaciafr@users.noreply.github.com>
2026-01-26 16:43:18 +0100

    Card System

---
+++ GGJMask/Assets/Scripts/ScriptKaci/CameraSystem/CameraViewer.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraViewer : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private Camera cam2;
+    [SerializeField] private Button Buttoncam;
+    [SerializeField] private Button Buttoncam2;
+
+    private void Start()
+    {
+        Buttoncam.onClick.AddListener(() => SwitchToCamera(cam));
+        Buttoncam2.onClick.AddListener(() => SwitchToCamera(cam2));
+
+        // Initialize - only first camera active
+        SwitchToCamera(cam);
+    }
+
+    private void SwitchToCamera(Camera targetCamera)
+    {
+        cam.gameObject.SetActive(cam == targetCamera);
+        cam2.gameObject.SetActive(cam2 == targetCamera);
+    }
+}
